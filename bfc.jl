eef001c8081496dac4dde19762f7d2db684324d0
Maël Karembe <mael.karembe@eleves.enpc.fr>
2022-04-28 22:51:47 +0200

    first draft of boundary fitted coordinates implementation finished

--- bfc.jl
+++ bfc.jl
@@ -11,19 +11,19 @@ else
 end
 
 macro dξ_1(A, i, j)
-    return :(($A[$i+1,$j]-$A[$i-1,$j])/2)
+    return :(($(esc(A))[$(esc(i))+1,$(esc(j))]-$(esc(A))[$(esc(i))-1,$(esc(j))])/2)
 end
 
 macro dξ_2(A, i, j)
-    return :(($A[$i+1,$j]-$A[$i-1,$j])/2)
+    return :(($(esc(A))[$(esc(i))+1,$(esc(j))]-$(esc(A))[$(esc(i))-1,$(esc(j))])/2)
 end
 
 macro dη_1(A, i, j)
-    return :(($A[$i,$j+1]-$A[$i,$j-1])/2)
+    return :(($(esc(A))[$(esc(i)),$(esc(j))+1]-$(esc(A))[$(esc(i)),$(esc(j))-1])/2)
 end
 
 macro dη_2(A, i, j)
-    return :(($A[$i,$j+1]-$A[$i,$j-1])/2)
+    return :(($(esc(A))[$(esc(i)),$(esc(j))+1]-$(esc(A))[$(esc(i)),$(esc(j))-1])/2)
 end
 
 function compute_u★!(nₓ,
@@ -43,7 +43,11 @@ function compute_u★!(nₓ,
                      g¹¹_2,
                      g¹²_2,
                      g²¹_2,
-                     g²²_2,                     
+                     g²²_2,
+                     g¹ₓ_1,
+                     g¹ᵧ_1,
+                     g²ₓ_2,
+                     g²ᵧ_2,
                      uₓ_1,
                      uᵧ_1,
                      uₓ_2,
@@ -76,12 +80,12 @@ function compute_u★!(nₓ,
     
 
     for i∈2:nₓ-1, j∈2:nᵧ-1
-        A1_1[i,j] = dt*(-(u¹_1*@dξ_1(uₓ_1,i,j)/dξ + u²_1*@dη_1(uₓ_1,i,j)/dη) + ν*J_1[i,j]*(@dξ_1(B11,i,j)/dξ + @dη_1(B12,i,j)/dη))
-        A2_2[i,j] = dt*(-(u¹_2*@dξ_1(uᵧ_2,i,j)/dξ + u²_2*@dη_2(uᵧ_2,i,j)/dη) + ν*J_2[i,j]*(@dξ_2(B21,i,j)/dξ + @dη_2(B22,i,j)/dη))
+        A1_1[i,j] = dt*(-(u¹_1[i,j]*@dξ_1(uₓ_1,i,j)/dξ + u²_1[i,j]*@dη_1(uₓ_1,i,j)/dη) + ν*J_1[i,j]*(@dξ_1(B11,i,j)/dξ + @dη_1(B12,i,j)/dη))
+        A2_2[i,j] = dt*(-(u¹_2[i,j]*@dξ_1(uᵧ_2,i,j)/dξ + u²_2[i,j]*@dη_2(uᵧ_2,i,j)/dη) + ν*J_2[i,j]*(@dξ_2(B21,i,j)/dξ + @dη_2(B22,i,j)/dη))
     end
 
-    A1_2[2:end-1,2:end-1] = 1/4*(A1_1[2:end,1:end-2] + A1_1[3:end-1,1:end-2] + A1_1[2:end,2:end] + A1_1[3:end-1,2:end])
-    A2_1[2:end-1,2:end-1] = 1/4*(A2_2[1:end-2,2:end] + A2_2[1:end-2,3:end-1] + A2_2[2:end,2:end] + A2_2[2:end,3:end-1])
+    A1_2[2:end-1,2:end-1] = 1/4*(A1_1[2:end-1,1:end-2] + A1_1[3:end,1:end-2] + A1_1[2:end-1,2:end-1] + A1_1[3:end,2:end-1])
+    A2_1[2:end-1,2:end-1] = 1/4*(A2_2[1:end-2,2:end-1] + A2_2[1:end-2,3:end] + A2_2[2:end-1,2:end-1] + A2_2[2:end-1,3:end])
 
     # boundary conditions on A1, A2 ?
 
@@ -93,7 +97,7 @@ function compute_u★!(nₓ,
     # u★¹_2, u★²_1 not needed to compute the divergence !!!
 end
 
-function compute_div_u★(nₓ,
+function compute_div_u★!(nₓ,
                         nᵧ,
                         dt,
                         dξ,
@@ -111,10 +115,64 @@ function compute_div_u★(nₓ,
         div_u★[i,j] = (ρ/dt)*(1/4)*(J_1[i,j]+J_1[i+1,j]+J_2[i,j]+J_2[i,j+1])*((u★¹_1[i+1,j]*inv_J_1[i+1,j]-u★¹_1[i,j]*inv_J_1[i,j])/dξ + (u★²_2[i,j+1]*inv_J_2[i,j+1]-u★²_2[i,j]*inv_J_2[i,j])/dη)
     end
 end
+
+function update_u!(nₓ,
+                   nᵧ,
+                   dt,
+                   dξ,
+                   dη,
+                   ρ,
+                   g¹¹_1,
+                   g¹²_1,
+                   g²¹_1,
+                   g²²_1,
+                   g¹¹_2,
+                   g¹²_2,
+                   g²¹_2,
+                   g²²_2,
+                   g₁ₓ_1,
+                   g₁ᵧ_1,
+                   g₁ₓ_2,
+                   g₁ᵧ_2,
+                   g₂ₓ_1,
+                   g₂ᵧ_1,                   
+                   g₂ₓ_2,
+                   g₂ᵧ_2,
+                   u★¹_1,
+                   u★²_2,
+                   p,
+                   u¹_1,
+                   u²_1,
+                   u¹_2,
+                   u²_2,
+                   uₓ_1,
+                   uᵧ_1,
+                   uₓ_2,
+                   uᵧ_2)
+
+    for i∈2:nₓ-1, j∈2:nᵧ-1
+        u¹_1[i,j] = u★¹_1[i,j] - (dt/ρ)*(g¹¹_1[i,j]*(p[i,j]-p[i-1,j])/dξ + g¹²_1[i,j]*((p[i,j+1]+p[i-1,j+1])/2-(p[i,j-1]+p[i-1,j-1])/2)/(2dη))
+        u²_2[i,j] = u★²_2[i,j] - (dt/ρ)*(g²¹_2[i,j]*((p[i+1,j]-p[i+1,j-1])/2-(p[i-1,j]-p[i-1,j-1])/2)/(2dξ) + g²²_2[i,j]*(p[i,j]-p[i,j-1])/dη)
+    end
+
+    u¹_2[2:end-1,2:end-1] = 1/4*(u¹_1[2:end-1,1:end-2] + u¹_1[3:end,1:end-2] + u¹_1[2:end-1,2:end-1] + u¹_1[3:end,2:end-1])
+    u²_1[2:end-1,2:end-1] = 1/4*(u²_2[1:end-2,2:end-1] + u²_2[1:end-2,3:end] + u²_2[2:end-1,2:end-1] + u²_2[2:end-1,3:end])    
+
+
+    @. uₓ_1 = u¹_1*g₁ₓ_1 + u²_1*g₂ₓ_1
+    @. uᵧ_2 = u¹_2*g₁ᵧ_2 + u²_2*g₂ᵧ_2
+
+    uₓ_2[2:end-1,2:end-1] = 1/4*(uₓ_1[2:end-1,1:end-2] + uₓ_1[3:end,1:end-2] + uₓ_1[2:end-1,2:end-1] + uₓ_1[3:end,2:end-1])
+    uᵧ_1[2:end-1,2:end-1] = 1/4*(uᵧ_2[1:end-2,2:end-1] + uᵧ_2[1:end-2,3:end] + uᵧ_2[2:end-1,2:end-1] + uᵧ_2[2:end-1,3:end])        
+    
+    # boundary conditions !!!
+    
+end
+                   
                         
 
 function NS_solve()
-    nₓ, nᵧ , nₜ = 30, 30, 10
+    nₓ, nᵧ , nₜ = 50, 50, 10
 
     w, h = 1.0, 1.0
     dξ, dη = w/nₓ, h/nᵧ
@@ -132,14 +190,7 @@ function NS_solve()
     u¹_2    = @zeros(nₓ, nᵧ)
     u²_2    = @zeros(nₓ, nᵧ)
 
-    # u★ₓ_1    = @zeros(nₓ, nᵧ)
-    # u★ᵧ_1    = @zeros(nₓ, nᵧ)
-    # u★ₓ_2    = @zeros(nₓ, nᵧ)
-    # u★ᵧ_2    = @zeros(nₓ, nᵧ)
-
     u★¹_1    = @zeros(nₓ, nᵧ)
-    # u★²_1    = @zeros(nₓ, nᵧ)
-    # u★¹_2    = @zeros(nₓ, nᵧ)
     u★²_2    = @zeros(nₓ, nᵧ)    
 
     p       = @zeros(nₓ, nᵧ)
@@ -175,17 +226,12 @@ function NS_solve()
     g¹ₓ_2   = @zeros(nₓ, nᵧ)
     g¹ᵧ_2   = @zeros(nₓ, nᵧ)
     g²ₓ_2   = @zeros(nₓ, nᵧ)
-    g²ᵧ_2   = @zeros(nₓ, nᵧ)                
-    
-    # g₁₁_1   = @ones(nₓ, nᵧ)
-    # g₁₂_1   = @zeros(nₓ, nᵧ)
-    # g₂₁_1   = @zeros(nₓ, nᵧ)
-    # g₂₂_1   = @ones(nₓ, nᵧ)
+    g²ᵧ_2   = @zeros(nₓ, nᵧ)
 
-    # g₁₁_2   = @ones(nₓ, nᵧ)
-    # g₁₂_2   = @zeros(nₓ, nᵧ)
-    # g₂₁_2   = @zeros(nₓ, nᵧ)
-    # g₂₂_2   = @ones(nₓ, nᵧ)
+    g₁ₓ_1   = @zeros(nₓ, nᵧ)
+    g₁ᵧ_1   = @zeros(nₓ, nᵧ)
+    g₂ₓ_2   = @zeros(nₓ, nᵧ)
+    g₂ᵧ_2   = @zeros(nₓ, nᵧ)
     
     g¹¹_1   = @ones(nₓ, nᵧ)
     g¹²_1   = @zeros(nₓ, nᵧ)
@@ -229,6 +275,16 @@ function NS_solve()
     @. g¹ᵧ_2 = -J_2*x˒η_2
     @. g²ₓ_2 = -J_2*y˒ξ_2
     @. g²ᵧ_2 = -J_2*x˒ξ_2
+
+    g₁ₓ_1 = x˒ξ_1
+    g₁ᵧ_1 = y˒ξ_1
+    g₁ₓ_2 = x˒ξ_2
+    g₁ᵧ_2 = y˒ξ_2
+
+    g₂ₓ_1 = x˒η_1
+    g₂ᵧ_1 = y˒η_1    
+    g₂ₓ_2 = x˒η_2
+    g₂ᵧ_2 = y˒η_2
 
     @. g¹¹_1 = g¹ₓ_1^2 + g¹ᵧ_1^2
     @. g¹²_1 = g¹ₓ_1*g²ₓ_1 + g¹ᵧ_1*g²ᵧ_1
@@ -311,30 +367,47 @@ function NS_solve()
             Δ[i,j,i-1,  j] += (1/4)*J/(dη*dξ)*g²¹_2[i,j]
             #Δ[i,j,  i,j-1] += (1/4)*J/(dη*dξ)*g²¹_2[i,j]
             Δ[i,j,i-1,j-1] += (1/4)*J/(dη*dξ)*g²¹_2[i,j]            
-        end
-
-        Δ = reshape(Δ, nₓ*nᵧ, nₓ*nᵧ)
-        
+        end    
     end
-
+    
+    Δ = reshape(Δ, nₓ*nᵧ, nₓ*nᵧ)
     # ------------------------------------------------------------------------------
 
-    # for i∈1:nₜ
-    #     compute_u★!(nₓ, nᵧ,
-    #                 dt, dξ, dη,
-    #                 ν,
-    #                 J_1, J_2, inv_J_1, inv_J_2,
-    #                 g¹¹_1, g¹²_1, g²¹_1, g²²_1, g¹¹_2, g¹²_2, g²¹_2, g²²_2,                     
-    #                 uₓ_1, uᵧ_1, uₓ_2, uᵧ_2, u¹_1, u²_1, u¹_2, u²_2, u★¹_1, u★²_2)
-    #     compute_div_u★(nₓ, nᵧ,
-    #                    dt, dξ, dη,
-    #                    ρ
-    #                    J_1, J_2, inv_J_1, inv_J_2,
-    #                    u★¹_1, u★²_2, div_u★)        
-
-    # end
+    for i∈1:nₜ
+        compute_u★!(nₓ, nᵧ,
+                    dt, dξ, dη,
+                    ν,
+                    J_1, J_2, inv_J_1, inv_J_2,
+                    g¹¹_1, g¹²_1, g²¹_1, g²²_1, g¹¹_2, g¹²_2, g²¹_2, g²²_2,
+                    g¹ₓ_1, g¹ᵧ_1, g²ₓ_2, g²ᵧ_2,                    
+                    uₓ_1, uᵧ_1, uₓ_2, uᵧ_2, u¹_1, u²_1, u¹_2, u²_2, u★¹_1, u★²_2)
         
-    return nₓ, nᵧ, Δ
+        compute_div_u★!(nₓ, nᵧ,
+                       dt, dξ, dη,
+                       ρ,
+                       J_1, J_2, inv_J_1, inv_J_2,
+                       u★¹_1, u★²_2, div_u★)
+
+        p = reshape(Δ \ reshape(div_u★, nₓ*nᵧ), nₓ, nᵧ)
+
+        update_u!(nₓ, nᵧ,
+                  dt, dξ, dη,
+                  ρ,
+                  g¹¹_1, g¹²_1, g²¹_1, g²²_1, g¹¹_2, g¹²_2, g²¹_2, g²²_2, g₁ₓ_1, g₁ᵧ_1, g₁ₓ_2, g₁ᵧ_2, g₂ₓ_1, g₂ᵧ_1, g₂ₓ_2, g₂ᵧ_2,
+                   u★¹_1, u★²_2,
+                   p,
+                   u¹_1, u²_1, u¹_2, u²_2, uₓ_1, uᵧ_1, uₓ_2, uᵧ_2)        
+        
+    end
+        
+    return nₓ, nᵧ, uₓ_1, uᵧ_2
 end
 
-nₓ, nᵧ, Δ = NS_solve()
+nₓ, nᵧ, uₓ_1, uᵧ_2 = NS_solve()
+
+x = repeat((0:(nₓ-1))*1/nₓ, 1, nᵧ)
+y = x'
+
+# egv = eigvecs(Δ)
+
+#PyPlot.plot_surface(x, y, reshape(egv[:,end], nₓ, nᵧ))
